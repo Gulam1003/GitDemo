dd247681d8c55f4fdcefe5169832426c2b988004
Gulam <Gulam@gmail.com>
2022-08-24 13:22:35 +0530

    From India

--- src/test/java/mavengroup/Mavenjava/AppiumTest.java
+++ src/test/java/mavengroup/Mavenjava/AppiumTest.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 public class AppiumTest {
 	@Test
 	public void login() {
-		System.out.println("This is Appium login");
+		System.out.println("This is Appium login1");
 	}
 	
 	@Test
